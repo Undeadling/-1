296c418bfe64c47cb0c244cf40d941255a892fdf
Undeadling <166484824+Undeadling@users.noreply.github.com>
2024-04-15 13:06:32 +0300

    Add files via upload

---
+++ Списки и словари ДЗ.py
@@ -0,0 +1,18 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi', 'pear', 'Pineapple']
+print(my_list)
+print(my_list[0])
+print(my_list[3])
+print(my_list[3:5])
+my_list[3] = 'lemon'
+print(my_list)
+my_dict = {}
+my_dict['apple'] = 'яблоко'
+my_dict['banana'] = 'банан'
+my_dict['orange'] = 'апельсин'
+print(my_dict)
+print(my_dict['apple'])
+print(my_dict['banana'])
+print(my_dict['orange'])
+my_dict['kiwi'] = 'киви'
+my_dict['apple'] = 'большое яблоко'
+print(my_dict)
